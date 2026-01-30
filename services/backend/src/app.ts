5e596d63e233534b15e4a8ef43c8c2d22443608a
Ashish Singh <ashishsingh4895@gmail.com>
2026-01-30 16:36:31 +0530

    Added local access url

--- services/backend/src/app.ts
+++ services/backend/src/app.ts
@@ -20,6 +20,7 @@ app.use(
   cors({
     origin: [
       process.env.FRONTEND_URL || 'http://localhost:5173',
+      'http://localhost:5173',
       'http://localhost:3000',
     ],
     credentials: true,
