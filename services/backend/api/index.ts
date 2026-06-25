1785a20c7cca354ebfc834af3d6672ae6e7bf5b4
Ashish Singh <ashishsingh4895@gmail.com>
2026-06-25 13:27:58 +0530

    fix(vercel): import express-async-errors in entrypoint to catch async errors correctly

--- services/backend/api/index.ts
+++ services/backend/api/index.ts
@@ -1,3 +1,4 @@
+import 'express-async-errors';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import app from '../src/app.js';
 import { connectDB } from '../src/config/database.js';
